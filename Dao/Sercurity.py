bfb1193c0f11729257bd73af7e4422c9e54d457c
Zechen6 <44701774+Zechen6@users.noreply.github.com>
2022-06-17 13:09:38 +0800

    bug修复
    修复了Sercurity中非字符类型的replace操作

--- Dao/Sercurity.py
+++ Dao/Sercurity.py
@@ -5,16 +5,19 @@ danger_letters = ['`', '@', '\"', '\'', '\\', '%', '_']
 def injection_attack_detector(entity):
     for key in entity.param_dict:
         for danger_letter in danger_letters:
-            entity.param_dict[key] = entity.param_dict[key].replace(danger_letter, "\\" + danger_letter)
+            if isinstance(entity.param_dict[key], str):
+                entity.param_dict[key] = entity.param_dict[key].replace(danger_letter, "\\" + danger_letter)
 
 
 def injection_attack_detector_single_param(param):
     for danger_letter in danger_letters:
-        param = param.replace(danger_letter, "\\" + danger_letter)
+        if isinstance(param, str):
+            param = param.replace(danger_letter, "\\" + danger_letter)
 
 
 def injection_attack_detector_param_list(param_list):
     for param in param_list:
         for danger_letter in danger_letters:
-            param = param.replace(danger_letter, "\\" + danger_letter)
+            if isinstance(param, str):
+                param = param.replace(danger_letter, "\\" + danger_letter)
 
